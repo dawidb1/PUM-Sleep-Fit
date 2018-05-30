2a33e700a0e82a58e806f68b5b6b92e225f85eee
Dawid Barański <dawid.baranski.96@gmail.com>
2018-05-30 16:42:21 +0200

    add step counter sync

--- app/src/main/java/com/example/dawid/projectpum/CommonBandSync.java
+++ app/src/main/java/com/example/dawid/projectpum/CommonBandSync.java
@@ -2,12 +2,16 @@ package com.example.dawid.projectpum;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -15,10 +19,21 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 
 import com.example.dawid.projectpum.DAL.Helpers.TimeFormatter;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSet;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.data.Value;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.sql.Time;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,11 +42,64 @@ import butterknife.OnClick;
 public class CommonBandSync extends AppCompatActivity {
 
     private int mHour, mMinute;
+    public static final String TAG = "StepCounter";
+    int GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 0533;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_common_band_sync);
         ButterKnife.bind(this);
+
+        FitnessOptions fitnessOptions = FitnessOptions.builder()
+                .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+                .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+                .build();
+        if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions)) {
+            GoogleSignIn.requestPermissions(
+                    this, // your activity
+                    GOOGLE_FIT_PERMISSIONS_REQUEST_CODE,
+                    GoogleSignIn.getLastSignedInAccount(this),
+                    fitnessOptions);
+        }
+        else {
+            readData();
+        }
+    }
+
+    private void readData() {
+        final ProgressDialog dialog=new ProgressDialog(this);
+            dialog.setMessage("Loading step data");
+        dialog.setCancelable(false);
+        dialog.setInverseBackgroundForced(false);
+        dialog.show();
+
+        Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .readDailyTotal(DataType.TYPE_STEP_COUNT_DELTA)
+                .addOnSuccessListener(
+                        new OnSuccessListener<DataSet>() {
+                            @Override
+                            public void onSuccess(DataSet dataSet) {
+                                long total =
+                                        dataSet.isEmpty()
+                                                ? 0
+                                                : dataSet.getDataPoints().get(0).getValue(Field.FIELD_STEPS).asInt();
+
+                                stepCount.setText(total+"");
+                                Log.i(TAG, "Total steps: " + total);
+                                dialog.hide();
+                                syncButton.setBackgroundColor(Color.GREEN);
+                                okText.setVisibility(View.VISIBLE);
+                                syncButton.setText("Zsynchronizowano");
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "There was a problem getting the step count.", e);
+                            }
+                        });
     }
 
     @OnClick(R.id.pickStartTime)
@@ -66,6 +134,19 @@ public class CommonBandSync extends AppCompatActivity {
         startActivity(intent);
     }
 
+    @OnClick(R.id.sync_button) void syncSteps(){
+        readData();
+    }
+
+    @BindView(R.id.ok_button)
+    TextView okText;
+
+    @BindView(R.id.step_count)
+    TextView stepCount;
+
+    @BindView(R.id.sync_button)
+    Button syncButton;
+
     @BindView(R.id.next_button)
     Button nextButton;
 
@@ -74,11 +155,4 @@ public class CommonBandSync extends AppCompatActivity {
 
     @BindView(R.id.pickEndTime)
     Button endTime;
-//
-//    @BindView(R.id.start_time_text)
-//    TextView startTime;
-//
-//    @BindView(R.id.end_time_text)
-//    TextView endTime;
-
 }
---
+++ app/src/main/java/com/example/dawid/projectpum/FitAPI/HistoryApi.java
@@ -0,0 +1,9 @@
+package com.example.dawid.projectpum.FitAPI;
+
+/**
+ * Created by Dawid on 30.05.2018.
+ */
+
+public class HistoryApi {
+
+}
